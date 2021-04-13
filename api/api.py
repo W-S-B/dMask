b4b70dff71105a2c5566bcd7e738712f451f7a79
TechXTT <71842793+TechXTT@users.noreply.github.com>
2021-04-13 22:13:05 +0300

    Made it faster by merging api with CheckFlow

--- api/api.py
+++ api/api.py
@@ -1,6 +1,54 @@
 from flask import Flask, render_template, Response
-from CheckFlow import get_frame
 import json
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+import cv2
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+cap = cv2.VideoCapture(0)  
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+MODEL_NAME = 'object_detection/new_graph' 
+
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+PATH_TO_LABELS = os.path.join('object_detection/data', 'label_map.pbtxt')
+
+NUM_CLASSES = 3
+
+classes = ['Mask', 'WrongMask', 'NoMask']
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
 app = Flask(__name__)
 
@@ -11,10 +59,32 @@ def api():
     }
 
 def gen():
-    while True:
-        frame = get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            while True:
+                ret, image_np = cap.read()
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                (boxes, scores, classes, num_detections) = sess.run(
+                    [boxes, scores, classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=6,
+                    min_score_thresh=.7)
+                ret, jpeg = cv2.imencode('.jpg', cv2.resize(image_np, (640, 480)))
+                frame = jpeg.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route("/history")
